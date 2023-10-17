fdc758669a74857d453a903fc17b27f12468ba3f
rossyen <rosselandjt1@gmail.com>
2023-10-17 18:08:57 +0200

    adds a repeating loop to program
    adds a loop that lets the user convert the temperature 5 times before it ends.

--- Python-programming-book/chapt2/temperature-converter.py
+++ Python-programming-book/chapt2/temperature-converter.py
@@ -2,11 +2,14 @@
 
 def main():
     print('This is a program that converts Celsius to Fahrenheit.')
-    celcius = int(input('What is the Celsius temperature? '))
-    fahrenheit = 9/5 * celcius + 32
+        
+    for i in range (5):
+        celsius = int(input('What is the Celsius temperature? '))
+        fahrenheit = 9/5 * celsius + 32
+        print('The temperature is {} degrees Fahrenheit.'.format(fahrenheit))   
     
-    print('The temperature is {} degrees Fahrenheit.'.format(fahrenheit))
 
 main()
 
 input('Press <Enter> key to quit.')
+
