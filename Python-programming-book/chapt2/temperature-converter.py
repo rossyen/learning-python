7b8d7b23dfd99085f893365538e780db134b0188
rossyen <rosselandjt1@gmail.com>
2023-10-17 18:36:30 +0200

    adds a new range loop that shows a list of converted integers
    adds a list that shows celsius in range 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
    converted to fahrenheit via loop and math.

--- Python-programming-book/chapt2/temperature-converter.py
+++ Python-programming-book/chapt2/temperature-converter.py
@@ -1,13 +1,20 @@
 # Celsius temps to Fahrenheit
 
 def main():
-    print('This is a program that converts Celsius to Fahrenheit.')
-        
+    print('This is a program that converts Celsius to Fahrenheit')
+    print('and shows a list from 0 to 100 Celsius converted in intervals of 10: ')
+
     for i in range (5):
         celsius = int(input('What is the Celsius temperature? '))
         fahrenheit = 9/5 * celsius + 32
         print('The temperature is {} degrees Fahrenheit.'.format(fahrenheit))   
     
+    temp_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    print('Lists temperature from celsius to fahrenheit in a list from 0 to 100 in intervals of 10: ')
+    for x in temp_list:
+        print(x,end='   ')
+        for i in range(1):
+            print(9/5 * int(x) + 32)
 
 main()
 
