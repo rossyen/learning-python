f841996ee5d593c493c2ab776cae7dd306dacfe3
rossyen <rosselandjt1@gmail.com>
2023-10-17 17:44:33 +0200

    adds welcoming description to file
    adds a welcome message to user of program

---
+++ Python-programming-book/chapt2/temperature-converter.py
@@ -0,0 +1,10 @@
+# Celsius temps to Fahrenheit
+
+def main():
+    print('This is a program that converts Celsius to Fahrenheit.')
+    celcius = int(input('What is the Celsius temperature? '))
+    fahrenheit = 9/5 * celcius + 32
+    
+    print('The temperature is {} degrees Fahrenheit.'.format(fahrenheit))
+
+main()
