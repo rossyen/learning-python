8f8838713efc9159d23138ccb96c0637d5c40abd
rossyen <rosselandjt1@gmail.com>
2023-10-18 15:18:08 +0200

    adds modification to file to let user select how many years to invest
    adds an input to let users decide how many years they want to invest
    and puts the number into the for i in range() so it cycles trough
    users input integer

--- Python-programming-book/chapt2/future-value2.py
+++ Python-programming-book/chapt2/future-value2.py
@@ -2,7 +2,7 @@
 def main():
 # Print introduction
     print('This program calculates the future value')
-    print('of a 10-year investment.')
+    print('of an investment over "x" years.')
 
 # Input the amount of the principal
     principal = int(input('Enter the initial principal: '))
@@ -10,12 +10,15 @@ def main():
 # Input the annual percentage rate
     apr = float(input('Enter the annual interest rate: '))
 
+    n = int(input('Enter how many years you want to save: '))
 # Repeat 10 times:
-    for i in range(10):
+    for i in range(n):
     # Principal = principal * (1 + apr)
         principal = principal * (1 + apr)
 
 # Output the value of principal
-    print('The value in 10 years is:', principal)
+    print(f'The value in {n} years is:', principal)
 
 main()
+
+input('Press \'enter\' key to exit program')
