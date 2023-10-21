e582621be9414af026a934807a156aae2da631f8
rossyen <rosselandjt1@gmail.com>
2023-10-21 09:33:49 +0200

    adds correct strings to user_input in bolean loop
    adds strings to the user_input part of loop insted of a variable.
    adds changes to formula in the for loop and removes the variable fahrenheit and replace
    with 'x' variable in temp_temp list.

--- Python-programming-book/chapt2/temp-converter2.py
+++ Python-programming-book/chapt2/temp-converter2.py
@@ -11,21 +11,21 @@ def main():
     print('If you want to see some random temperature conversions type "n" ')
 
 # Receive input and store in variable    
-    y, n = (input('Else press "enter" to exit program: ')).split()
+    user_input = str(input('Else press "enter" to exit program: '))
 
 # if answer is qual to y convert fahrenheit to celsius
-    if (y == str(y)):
+    if (user_input == 'y'):
         fahrenheit = int(input('Type in fahrenheit to convert to celsius: '))
         celsius = (fahrenheit-32)/1.8 
         print(f'{fahrenheit} degrees fahrenheit equals {celsius:.2f} degrees celsius ')
-        return
+
 # if answer is equal to n show list that converts for user 
-    elif (n == str(n)):
-        temp_list = [-10, -5, 0, 5, 10, 15, 20, 25, 30]
+    elif (user_input == 'n'):
+        temp_list = [-10, 0, 10, 20, 30, 40, 50, 60, 70]
         for x in temp_list:
             print(x,end='   ')
             for i in range(1):
-                print((float((fahrenheit)-32)/1.8))
+                print((int((x)-32)/1.8))
         return
 # else return to menu    
     else:
