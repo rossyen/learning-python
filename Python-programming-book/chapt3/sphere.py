474d86d9adf5d84e94852d26d06c80b742ce0b9e
rossyen <rosselandjt1@gmail.com>
2023-10-23 18:33:13 +0200

    adds a temp file for an assignment while i restart vscode

---
+++ Python-programming-book/chapt3/sphere.py
@@ -0,0 +1,21 @@
+# sphere.py
+#   A program that calculates the volume and surface area
+#   of a sphere from its radius, given as input
+
+import math
+
+def volume_area_sphere()
+
+#   Print a welcoming message to user
+    print('This is a program that shows you both the volume and area of a sphere')
+
+#   Get input from user on radius on sphere
+    radius = int(input('Please enter the radius of you sphere to see volume and surface: '))
+
+#   Calculate volume
+    volume = 4 / 3 * math.pi * radius**3
+
+#   Calculate area
+
+#   Print results
+
