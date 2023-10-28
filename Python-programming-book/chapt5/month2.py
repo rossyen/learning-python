caf6cce890631a96219ee71a8da61b681a568dd6
rossyen <rosselandjt1@gmail.com>
2023-10-28 17:05:42 +0200

    adds a month abbreviation script to program
    adds a list of all months and lets user input number 1-12 to print
    out month from list

---
+++ Python-programming-book/chapt5/month2.py
@@ -0,0 +1,18 @@
+# month2.py
+#   A program to print the month abbreviation, given its number.
+
+def main():
+
+    # months is a list used as a lookup table
+    months = ["January", "February", "March" , "April " ,
+              "May" , "June ", "July", "August ",
+              "September", "October" , "November", "December"]
+    
+    n =int(input('Enter a month number (1-12): '))
+    print()
+    
+    print('The month abbreviation is', months[n-1] + '. \n')
+
+main()    
+
+input('Press enter to exit.')
