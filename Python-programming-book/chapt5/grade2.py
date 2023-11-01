d8821e288a96233e44b065c851a4bbe585a16a3d
rossyen <rosselandjt1@gmail.com>
2023-11-01 17:02:39 +0100

    adds a script to grade exams with score input and grade output

--- Python-programming-book/chapt5/grade2.py
+++ Python-programming-book/chapt5/grade2.py
@@ -1,3 +1,31 @@
 # grade2.py
 #   A program to grade 100-point exams on a the scale
 #   90-100:A, 80-89:B, 70-79:C, 60-69:D, <60:F.
+
+
+def loop():
+    loop = input('Type "y" to try again, else press enter to exit\n')
+    print()
+    if loop == ('y'):
+        return main()
+    else:
+        exit
+
+def main():
+    
+    # receive input from user on how many points on exam
+    score = int(input('What score did you get on exam?\n'))
+    
+    # make a variable of grades and multiply each string so it fits the score with number
+    # of letter in each score. 
+
+    grades = 60*'F'+10*'D'+10*'C'+10*'B'+11*'A'
+
+    # print out the grade with a list manipulation
+    # the manipulation makes the score variable select the number
+    # the characters have in the grade string.
+    print (f'The grade on your exam is {grades[score]}')
+    
+    loop()
+
+main()
