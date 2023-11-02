c09adb7262b3df47512a21f019219eb40617ac8c
rossyen <rosselandjt1@gmail.com>
2023-11-02 19:01:54 +0100

    addsa a script to get the value of a single name

--- Python-programming-book/chapt5/name-value.py
+++ Python-programming-book/chapt5/name-value.py
@@ -2,3 +2,23 @@
 #   A program to determinate a person's character traits based on the
 #   numeric value of a name
 
+def loop():
+    loop = input('Type "y" to try again, else press enter to exit\n')
+    print()
+    if loop == ('y'):
+        return main()
+    else:
+        exit
+
+
+def main():
+    
+    name = input('Enter a single name: ')
+    total = 0
+    for letter in name:
+        total = total + ord(letter.lower()) - ord('a') + 1
+    print(f'The value is: {total}')
+    loop()    
+main()
+
+    
