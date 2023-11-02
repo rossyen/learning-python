2d21332f2264120c50018b0ab96b691163a7ca9b
rossyen <rosselandjt1@gmail.com>
2023-11-02 19:13:50 +0100

    adds a script to see number value of many names

---
+++ Python-programming-book/chapt5/name-value2.py
@@ -0,0 +1,29 @@
+# name-value.py
+#   A program to determinate a person's character traits based on the
+#   numeric value of a name
+
+def loop():
+    loop = input('Type "y" to try again, else press enter to exit\n')
+    print()
+    if loop == ('y'):
+        return main()
+    else:
+        exit
+
+
+def main():
+    
+    name = input('Enter a name: ')
+    namesplit = name.split(' ')
+    fullnamelist = [word[:] for word in namesplit]
+    fullname = ''.join(fullnamelist)
+    print(fullname)
+
+    total = 0
+    for letter in fullname:
+        total = total + ord(letter.lower()) - ord('a') + 1
+    print(f'The value is: {total}')
+    loop()    
+main()
+
+    
