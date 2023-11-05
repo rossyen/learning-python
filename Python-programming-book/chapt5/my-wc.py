adb4bee559c2bfa4ecf42e5d65b51c68657c45c5
rossyen <rosselandjt1@gmail.com>
2023-11-05 18:21:05 +0100

    WIP

---
+++ Python-programming-book/chapt5/my-wc.py
@@ -0,0 +1,16 @@
+# my-wc.py
+# a program that analyse a file to determine the number of 
+# lines, words, and characters contained therein.
+
+# Into to program
+
+# accept a file name as input
+
+
+# count number of lines
+
+# count number of words
+
+# count number of characters 
+
+# print three numbers showing count of lines, words, and characters in the file
