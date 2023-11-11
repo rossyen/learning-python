ee68d07c7255075a2e5f2b506be42363c126445c
rossyen <rosselandjt1@gmail.com>
2023-11-11 22:37:56 +0100

    adds change to code to make it easier to read
    changes variables and calculations of pizza area and price pr square
    to functions that are called for in main() for easier readability
    
    removes comments in code because new functions define good enough what
    the code is going to do

--- Python-programming-book/chapt3/pizza-price.py
+++ Python-programming-book/chapt3/pizza-price.py
@@ -3,23 +3,27 @@
 #   of a circular pizza.
 
 
-import math # import math
-def cost_square_meter():
+import math 
+
+def areaPizza(radius):
+    area = math.pi * (radius/2)**2
+    return area
+
+def costSquareMeterPizza(price, radius):
+    costSquare = price / areaPizza(radius)
+    return costSquare
+
+def main():
 
 # print information to user
     print('This is a program that calculates the cost per square meter of your pizza.')
     
-# ask for price of pizza and store value as integer
-    price_pizza =int(input('How much did you pay for your pizza in NOK? '))
+    price =int(input('How much did you pay for your pizza in NOK? '))
+    radius = int(input('How big is your pizza (in CM)? '))
 
-# ask for size of pizza in cm and store value as integer
-    size_pizza = int(input('How big is your pizza (in CM)? '))
+    # pizzaSize = areaPizza(radius)
+    costPizza = costSquareMeterPizza(price, radius)
 
-# calculate cost per square meter
-    area_pizza = math.pi * (size_pizza/2)**2
-    price_area_pizza = price_pizza / area_pizza
+    print(f'Your pizza costs {costPizza:.2f} kr. pr square meter.')
 
-# print result to user
-    print(f'Your pizza costs {price_area_pizza:.2f} kr. pr square meter.')
-
-cost_square_meter()
+main()
