ab25da8cd174c9f7b192a6b03900b5885f8ee844
rossyen <rosselandjt1@gmail.com>
2023-11-12 09:07:36 +0100

    adds changes to main code to create a fucntion for more readability
    adds a function to calculate the fibonacci sequene
    adds a call to funciton in main()
    removes fibonacci code from main()
    cleans up code for more readability

--- Python-programming-book/chapt3/fibonacci-sequence.py
+++ Python-programming-book/chapt3/fibonacci-sequence.py
@@ -3,16 +3,18 @@
 #   'n' is a value input by user.
 # example: if n = 6, then the result is 8.
 
-def fib_seq():
+def fibonacciSequence(n):
+    a = 0
+    b = 1
+    for i in range (n-1):
+        a,b=b,a+b
+    return b
+
+def main():
     print('This program lets you find the value of a number in the Fibonacci sequence.')
     print()
 
     n = int(input('Enter number in sequence to see value of number: '))
-    a = 0
-    b = 1
-    for i in range(n-1):
-        a,b=b,a+b
+    print(fibonacciSequence(n))
 
-    print(b)
-
-fib_seq()
+main()
