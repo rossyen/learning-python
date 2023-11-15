4f8fecc1425a2a7a762d0575b9a38341da410876
rossyen <rosselandjt1@gmail.com>
2023-11-15 19:20:28 +0100

    adds a function to convert temperature and ands two if statements
    adds a function to convert celcius to fahrenheit and returns fahrenheit
    adds a variable called fahrenheit to call for function
    adds two if statements to print out messages for extreme temperatures

--- Python-programming-book/chapt2/temperature-converter.py
+++ Python-programming-book/chapt2/temperature-converter.py
@@ -1,22 +1,34 @@
 # Celsius temps to Fahrenheit
 
-def main():
-    print('This is a program that converts Celsius to Fahrenheit')
-    print('and shows a list from 0 to 100 Celsius converted in intervals of 10: ')
+def celsiusToFahrenheit(celsius):
+    fahrenheit = 9.0/5.0 * celsius + 32
+    return fahrenheit
 
-    for i in range (5):
-        celsius = int(input('What is the Celsius temperature? '))
-        fahrenheit = 9.0/5.0 * celsius + 32
-        print('The temperature is {} degrees Fahrenheit.'.format(fahrenheit))   
+
+def main():
     
+    celsius = int(input('What is the Celsius temperature? '))
+    fahrenheit = celsiusToFahrenheit(celsius)
+    print(f'The temperature is {fahrenheit:.2f} degrees Fahrenheit.')   
+    
+    # Print warming for extreme temps
+    if fahrenheit > 90:
+        print("It's really hot out there. Be careful!")
+    if fahrenheit < 30:
+        print("Brrrrr. Be sure to dress warmly")
+
+    '''
+    Commented out temperature list.
+
     temp_list = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     print('Lists temperature from celsius to fahrenheit in a list from 0 to 100 in intervals of 10: ')
     for x in temp_list:
         print(x,end='   ')
         for i in range(1):
             print(9.0/5.0 * int(x) + 32)
+'''
 
 main()
 
-input('Press <Enter> key to quit.')
+
 
