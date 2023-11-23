d4d886e923dbc794906de2c19b94d8e2ea9ceee7
rossyen <rosselandjt1@gmail.com>
2023-11-23 19:00:29 +0100

    adds code to calculate corresponding day number and fixes small errors in function
    adds code in main to calculate correpsonding day number and print result
    fixes bug typos in function validateDate where "==" was supposed to be "in"

--- Python-programming-book/chapt7/validateDates.py
+++ Python-programming-book/chapt7/validateDates.py
@@ -49,24 +49,39 @@ def validateDate(day, month, year):
             else:
                 return("Invalid date.")
         elif leapYear(year) == True:
-            if day >= 1 and day <= 31 and month == [1, 3, 5, 7, 8, 10, 12]:
+            if day >= 1 and day <= 31 and month in [1, 3, 5, 7, 8, 10, 12]:
                 return(f"Valid date of {day:02}/{month:02}/{year:04}.")
             elif day >= 1 and day <= 29 and month == 2:
                 return(f"Valid date of {day:02}/{month:02}/{year:04}.")
-            elif day >= 1 and day <= 28 and month [4, 6, 9, 11]:
+            elif day >= 1 and day <= 28 and month in [4, 6, 9, 11]:
                 return(f"Valid date of {day:02}/{month:02}/{year:04}.")
             else:
                 return("Invalid date.")
-
-    
 
 def main():
     
     try:
         day, month, year = input("dd/mm/yyyy: ").split("/")
+        day = int(day)
+        month = int(month)
+        year = int(year)
         print(validateDate(day, month, year))
 
 
+        # Prints corresponding day number in the year. 
+        dayNum = 31*(month - 1) + day
+        
+        if month > 3 and month <= 12:
+            dayNum = dayNum - (4*(month)+23)//10
+            if leapYear(year) == True and month > 2 and month <=12:
+                dayNum = dayNum + 1
+                print(f"Corresponding day number is {dayNum}.")
+            else:
+                print(f"Corresponding day number is {dayNum}.")
+        else:
+            print(f"Corresponding day number is {dayNum}.")
+            
+        
         # Calculate the corresponding day number after validating the date
         # three steps using int arithmetic: 
         # dayNum = 31(month - 1) + day
