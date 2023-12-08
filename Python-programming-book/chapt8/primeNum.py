612a4e4f18036fd34b04204351f850ed4fde4c71
rossyen <rosselandjt1@gmail.com>
2023-12-08 17:44:59 +0100

    WIP

---
+++ Python-programming-book/chapt8/primeNum.py
@@ -0,0 +1,24 @@
+# primeNum.py
+
+
+# A positive whole number n > 2 is prime if no number
+# between 2 and square root (n) evenly divides n.
+import math
+
+
+
+
+def prime(n):
+# determine if the value is prime
+# if n is not prime, program should quit as soon
+# as it finds a value that evenly divides n
+
+
+
+def main():
+# Accept value of n as input
+    n = int(input("Enter a positive whole number to see if it is a prime number: "))
+    print(prime(n))
+
+
+main()
