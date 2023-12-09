4cb9ca3fd728d37bd5432499bbe6815db637e78f
rossyen <rosselandjt1@gmail.com>
2023-12-09 14:49:07 +0100

    adds a function with a loop to append prime number up to and including input
    adds a function to append prime numbers from 2 up to and including input n if prime.
    the function returns the appended sequence.
    adds a call to function in main to print out appended list

--- Python-programming-book/chapt8/primeNum.py
+++ Python-programming-book/chapt8/primeNum.py
@@ -5,6 +5,9 @@
 # as input and determines if the value is prime. If n it not prime,
 # your program should quit as soon as it finds a value that evenly divides n.
 
+
+# modify program to find every prime number less than or equal to n
+
 import math
 
 def isPrime(n):
@@ -23,16 +26,27 @@ def isPrime(n):
     # If no divisor found, it's a prime number
     return True
 
+def primeNumberList(n):
+    sequence = []
+    for i in range(2, n + 1):
+        if isPrime(i):
+            sequence.append(i)
+    return sequence
+
 def main():
     
     n = int(input("Enter a number to see if its a prime number or not: "))
 
     num_to_check = n
 
+
     if isPrime(num_to_check):
         print(f'{n} is a prime number.')
     else:
         print(f'{n} is not a prime number')
+
+    print(f'All these numbers are prime numbers up to {n} and including if {n} is prime: \n{primeNumberList(n)}')
+    
 
 if __name__ == '__main__':
     main()    
