b1fcc79948a60f35e18a946225ab72f77f1d4a01
rossyen <rosselandjt1@gmail.com>
2023-12-13 17:33:26 +0100

    adds new function and changes old
    adds a change to function wordReversal and renames it to meet new changes
    adds a True False function on palindromeTest
    adds a stripString function to clean string of commas, punctuation and whitespaces
    adds if else statements to check if input is even or odd in lenght
    adds print information to user of program

--- Python-programming-book/ChatGPT-Exercises/palindromeChecker.py
+++ Python-programming-book/ChatGPT-Exercises/palindromeChecker.py
@@ -14,21 +14,53 @@
 
 
 
-def wordReversal(sentence):
-    sentence = sentence
-    reversedSentence = sentence.split()
-    reversedSentence = reversedSentence[::-1]
-    reversedSentence = " ".join(reversedSentence)
-    return reversedSentence
+def reverseString(string):
+    string = string[::-1]
+    string = "".join(string) # string is receives as a list, thats why this is here
+    return string
 
+def palindromeTest(one, two):
+    return one == two
+
+def stripString(string):
+    string = string.lower()
+    strippedstring = string.replace(".", "")
+    strippedstring = strippedstring.replace(",", "")
+    strippedstring = strippedstring.replace(" ", "")
+    return strippedstring
 
 def main():
-    palindrome = str(input("Enter a word, phrase, number, or other sequence of characters\n to see if its a palindrome or not.\n:"))
-    palindrome = palindrome.lower()
-    strippedPalindrome = palindrome.replace(".", "")
-    strippedPalindrome = strippedPalindrome.replace(",", "")
-    strippedPalindrome = strippedPalindrome.replace(" ", "")
+    print("This program lets you see if a word, sentence, number, phrase or a sequence of characters is a palindrome or not.")
+    print("Example: The name Otto is spelled Otto backwards.\nThis program excludes spaces, commas, punctuation and uppercase letters.")
+    print("So for example if you write: \"ex.t t, ttxe\" it will print out that \"ex.t t, ttxe\" is a Palindrome, since \"exttttxe\" is a Palindrome.\n")
+    palindrome = str(input("Input: "))
 
-    print(strippedPalindrome, len(strippedPalindrome))
 
-main()
+    
+    
+    # making letters into a list
+    palindromeList = []
+    for letter in stripString(palindrome):
+        palindromeList.append(letter)
+    # if palindrome is even number split in two
+    if len(palindromeList) % 2 == 0:
+        evenList = len(palindromeList) // 2
+        split = reverseString(palindromeList[0:evenList])
+        split2 = "".join(palindromeList[evenList:])
+        if palindromeTest(split, split2):
+            print(f"{palindrome} is a Palindrome")
+        else:
+            print(f"{palindrome} is NOT a Palindrome")
+
+    # if palindrome is uneven: separate the middle number and split rest in two 
+    elif len(palindromeList) % 2 != 0:
+        oddList = len(palindromeList) // 2
+        split = reverseString(palindromeList[0:oddList])
+        split2 = "".join(palindromeList[oddList + 1:])
+        if palindromeTest(split, split2):
+            print(f"{palindrome} is a Palindrome")
+        else:
+            print(f"{palindrome} is NOT a Palindrome")
+
+if __name__ == "__main__":
+    main()
