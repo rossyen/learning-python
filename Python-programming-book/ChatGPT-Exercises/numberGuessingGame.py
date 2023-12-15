d74355d5e8feaceab7e9a4abd290afdafcd1fea4
rossyen <rosselandjt1@gmail.com>
2023-12-15 17:57:42 +0100

    adds a try/except to main if input is not an integer
    adds a try/except to make user enter a number between 1 and 100
    adda a if else restart input on both try and except

--- Python-programming-book/ChatGPT-Exercises/numberGuessingGame.py
+++ Python-programming-book/ChatGPT-Exercises/numberGuessingGame.py
@@ -15,36 +15,51 @@ def randomNumber():
 
 
 def main():
-    print("This program lets you guess a random number in the range of 1 to 100 in 9 attempts")
-    guess = int(input("Whats your guess: "))
-    numberToGuess = randomNumber()
-    attempts = 0
+    print(f"This program lets you guess a random number in the range of 1 to 100 in 9 attempts")
 
-    print()
-
-    # I've commented the 3 places you need to change, when changing number of attempts in this game.
-    while True:
-        attempts += 1
+    try:
         
-        if attempts <7: # change number here
-            if guess == numberToGuess:
-                print ("Correct!")
+        guess = int(input("Whats your guess: "))
+        numberToGuess = randomNumber()
+        attempts = 0
+
+        print()
+
+        # I've commented the 3 places you need to change, when changing number of attempts in this game.
+    
+        while True:
+            attempts += 1
+        
+            if attempts <7: # change number here
+                if guess == numberToGuess:
+                    print ("Correct!")
+                    break
+                elif guess < numberToGuess:
+                    print ("\nThe number is to low\n")
+                elif guess > numberToGuess:
+                    print ("\nThe number is to high\n")
+                guess = int(input("Guess again: "))
+        
+            elif attempts == 8: # change number here
+                print("\nYou have one attempt left.\n")
+                guess = int(input("Guess again: "))
+            elif  attempts == 9: # change number here
+                print("\nYou guessed wrong and have used all your attempts. You lose.\n")
                 break
-            elif guess < numberToGuess:
-                print ("\nThe number is to low\n")
-            elif guess > numberToGuess:
-                print ("\nThe number is to high\n")
-            guess = int(input("Guess again: "))
         
-        elif attempts == 8: # change number here
-            print("\nYou have one attempt left.\n")
-            guess = int(input("Guess again: "))
-
-
-        elif  attempts == 9: # change number here
-            print("\nYou guessed wrong and have used all your attempts. You lose.\n")
-            break
+        quitRestart1 = input("\nIf you want to play again press <Enter>, or type in \"q\" or anything else and enter to quit.\n: >> ")
+        if quitRestart1 == "":
+            main()
+        else:
+            quit()
         
+    except ValueError:
+        print("\nPlease enter a number between 1 and 100 to play this game.")
+        quitRestart2 = input("Press <Enter> to restart, or type in \"q\" and enter to quit.\n: >> ")
+        if quitRestart2 == "":
+            main()
+        else:
+            quit()
         
 if __name__ == "__main__":
     main()
